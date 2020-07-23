fb76292023ab4f2ee5df1ea9d7b24914ed70a51e
olesya.p <olesya.p@digital-spectr.ru>
2020-07-23 23:01:26 +0500

    Updates

---
+++ precache-manifest.f8c9da30b3db85afe63b507b27fe8b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9440d1dad22b70fb7d4fcbb8db25dcd",
+    "url": "/scripts-list/index.html"
+  },
+  {
+    "revision": "f1b5f58860ec6605dce8",
+    "url": "/scripts-list/static/css/main.fd8ea865.chunk.css"
+  },
+  {
+    "revision": "356e16a969dd51b1bd56",
+    "url": "/scripts-list/static/js/2.9c2c20e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/scripts-list/static/js/2.9c2c20e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b5f58860ec6605dce8",
+    "url": "/scripts-list/static/js/main.c0c6fd10.chunk.js"
+  },
+  {
+    "revision": "3b01d3e5023321a70d20",
+    "url": "/scripts-list/static/js/runtime-main.a2b01dc8.js"
+  }
+]);
